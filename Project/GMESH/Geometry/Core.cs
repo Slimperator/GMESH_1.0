a8ca74d1aa5d8341522167eba23d6fb18289444f
AlexFedotov <alesha.school127@bk.ru>
2016-03-21 14:17:30 +0400

    Added Project

---
+++ Project/GMESH/Geometry/Core.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    public interface IPoint
+    {
+        double X { get; set; }
+        double Y { get; set; }
+    }
+
+    public interface ISegment
+    {
+        IPoint st { get; set; }
+        IPoint fin { get; set; }
+    }
+
+    public interface Curve
+    {
+        void getPoint(double t, out IPoint p);
+    }
+}
