e00dbffb6a1a87fbd8c4e4d50f21a03cd240c227
Ani <kirakosyan.ani@mail.ru>
2016-04-02 17:27:08 +0400

    Добавила класс кривой Безье

--- Project/GMESH/Geometry/Bezier.cs
+++ Project/GMESH/Geometry/Bezier.cs
@@ -4,13 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Geometry
 {
     public class Bezier : ICurve
     {
+        private IPoint P0;
+        private IPoint P1;
+        private IPoint P2;
+        private IPoint P3;
+       
+        public Bezier(IPoint P0, IPoint P1, IPoint P2, IPoint P3)
+        {
+         this.P0 = new Point(P0.X, P0.Y);
+         this.P1 = new Point (P1.X, P1.Y);
+         this.P2 = new Point (P2.X, P2.Y);
+         this.P3 = new Point(P3.X, P3.Y);
+        }
+      
         public void getPoint(double t, out double x, out double y)
         {
-            throw new NotImplementedException();
+           
+            x = this.P0.X * (1 - t) * (1 - t) * (1 - t) + 3 * t * P1.X * (1 - t) * (1 - t) + 3 * t * t * P2.X * (1 - t) + t * t * t * P3.X;
+            y = this.P0.Y * (1 - t) * (1 - t) * (1 - t) + 3 * t * P1.Y * (1 - t) * (1 - t) + 3 * t * t * P2.Y * (1 - t) + t * t * t * P3.Y;
+
+        
         }
     }
 }
