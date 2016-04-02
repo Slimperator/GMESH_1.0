b627c1dd82edbf3b3a20c043c06e3d786d3bad36
Ani <kirakosyan.ani@mail.ru>
2016-04-02 17:30:50 +0400

    Добавила отрисовку Безье

--- Project/GMESH/GMESH/Form1.cs
+++ Project/GMESH/GMESH/Form1.cs
@@ -25,6 +25,7 @@ namespace GMESH
         double h = 0.01;
         IPoint center;
         ICurve curve;
+        IPoint P0, P1, P2, P3; // для Безье
 
         private void DrawAstroid()
         {
@@ -48,11 +49,29 @@ namespace GMESH
             y = 300;
             center = new Geometry.Point(x, y);
             curve = new Cardioid(center, radius);
-            for (t = 0; t < 7; t += h)
+            for (t = 0; t < 6.5; t += h)
             {
                 curve.getPoint(t, out x1, out y1);
                 curve.getPoint(t + h, out x2, out y2);
                 g.DrawLine(new Pen(Color.Purple), (int)x1, (int)y1, (int)x2, (int)y2);
+            }
+        }
+        private void DrawBezier()
+        {
+            Graphics g = this.CreateGraphics();
+           
+            P0 = new Geometry.Point(200, 200);
+            P1 = new Geometry.Point(300, 100);
+            P2 = new Geometry.Point(400, 100);
+            P3 = new Geometry.Point(500, 200);
+            curve = new Bezier(P0, P1, P2, P3);
+
+            for (t = 0; t < 1; t += h)
+            {
+                curve.getPoint(t, out x1, out y1);
+                curve.getPoint(t + h, out x2, out y2);
+                g.DrawLine(new Pen(Color.Green), (int)x1, (int)y1, (int)x2, (int)y2);
+
             }
         }
 
@@ -60,6 +79,7 @@ namespace GMESH
         {
             DrawAstroid();
             DrawCardioid();
+            DrawBezier();
         }
     }
 }
