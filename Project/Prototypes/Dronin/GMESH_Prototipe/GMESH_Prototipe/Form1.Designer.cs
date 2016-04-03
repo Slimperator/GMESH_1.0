1c9a8d6d9229bc2f2e3feb3fc9dd6ff1f7989189
Slimperator <slimperator@gmail.com>
2016-04-03 23:38:26 +0300

    create folder Dronin with Prototype in GMESH_1.0/Project/Prototypes

---
+++ Project/Prototypes/Dronin/GMESH_Prototipe/GMESH_Prototipe/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace GMESH_Prototipe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exiteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(633, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exiteToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(50, 20);
+            this.toolStripMenuItem1.Text = "Menu";
+            // 
+            // exiteToolStripMenuItem
+            // 
+            this.exiteToolStripMenuItem.Name = "exiteToolStripMenuItem";
+            this.exiteToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.exiteToolStripMenuItem.Text = "Exite";
+            this.exiteToolStripMenuItem.Click += new System.EventHandler(this.exiteToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 428);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exiteToolStripMenuItem;
+    }
+}
+
---
+++ Project/Prototypes/Dronin/GMESH_Prototipe/GMESH_Prototipe/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GMESH_Prototipe
+{
+    public partial class Form1 : Form
+    {
+        private List<Point> Points = new List<Point>();
+        private Graphics G;
+        private bool Mouse_State = false;
+        private int Point_Index = -1;
+        public Form1()
+        {
+            InitializeComponent();
+            G = this.CreateGraphics();
+        }
+
+        private void exiteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+
+                    foreach (Point P in Points)
+                    {
+                        if (Math.Pow((P.X - e.X), 2) + Math.Pow((P.Y - e.Y), 2) <= Math.Pow(P.R, 2))
+                        {
+                            Point_Index = Points.IndexOf(P);
+                            break;
+                        }
+                    }
+
+                    if(Point_Index != -1)
+                    {
+                        break;
+                    }
+
+                    Point Point = new Point(e.X, e.Y);
+                    Points.Add(Point);
+                    this.Invalidate();                               
+                    break;
+
+                case MouseButtons.Right:
+                    break;
+            }                
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Mouse_State = true;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Point P in Points)
+            {
+                P.ShowPoint(G, this);
+                G.DrawString( 
+                    Convert.ToString(Points.IndexOf(P)),
+                    new Font(
+                       new FontFamily("Arial"),
+                       10,
+                       FontStyle.Regular,
+                       GraphicsUnit.Pixel),
+                       new SolidBrush(Color.Black),
+                       (P.X + P.R), (P.Y - P.R))
+                       ;
+                if (Points.IndexOf(P)>=1)
+                {
+                        G.DrawLine(new Pen(new SolidBrush(Color.Red), 1), Points[Points.IndexOf(P) - 1].X, Points[Points.IndexOf(P) - 1].Y, P.X, P.Y);
+                        if (Points.IndexOf(P) == Points.Count - 1)
+                            G.DrawLine(new Pen(new SolidBrush(Color.Red), 1), P.X, P.Y, Points[0].X, Points[0].Y);               
+                }
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Mouse_State == true && Point_Index != -1)
+            {
+                Points[Point_Index].X = e.X;
+                Points[Point_Index].Y = e.Y;
+                this.Invalidate();                            
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Mouse_State = false;
+            Point_Index = -1;
+        }
+    }
+
+    public class Point
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float R = 10;
+
+
+        public Point(float X, float Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public void ShowPoint (Graphics g, Form Form)
+        {
+            g.DrawEllipse(new Pen(new SolidBrush(Color.Red), 1), X-R, Y - R, 2*R, 2*R);          
+        }
+    }
+}
