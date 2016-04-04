f0cc5f467dbb74b3c1617de1bc78666db0198f44
Elena Krotova <ekrotovva@gmail.com>
2016-04-04 20:42:53 +0300

    Дополнила класс Line

--- Project/GMESH/Geometry/Line.cs
+++ Project/GMESH/Geometry/Line.cs
@@ -8,9 +8,22 @@ namespace Geometry
 {
     public class Line : ICurve
     {
+        private IPoint L1;
+        private IPoint L2;
+
+        public Line(IPoint L1, IPoint L2)
+        {
+            this.L1 = new Point(L1.X, L1.Y);
+            this.L2 = new Point(L2.X, L2.Y);
+            
+        }
         public void getPoint(double t, out double x, out double y)
         {
-            throw new NotImplementedException();
+            x = this.L1.X + (1 - t) * L2.X;
+            y = this.L1.Y + (1 - t) * L2.Y;
+
+            //x = this.L1.X * (1 - t) + t * L2.X;
+            //y = this.L1.Y * (1 - t) + t * L2.Y;
         }
     }
 }
