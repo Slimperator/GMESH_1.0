20fc672b99b96ad59816345e6c658c83dd121dc2
DariaGorschkova <gorschkova15daria@yandex.ru>
2016-04-05 23:25:19 +0300

    Добавила Parser

---
+++ Project/Parser/Parser/Parser/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Soap;
+
+namespace Parser_050416
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(gmesh));
+            Stream f;
+
+            f = File.OpenRead("gmesh.xml");
+            gmesh g = (gmesh)xml.Deserialize(f);
+            f.Close();
+            Console.WriteLine(g);
+
+
+
+
+
+            point[] ps1 = new point[]
+            {
+                new point(point.type_point.st, 10, 10),
+                new point(point.type_point.fin, 20, 20)
+            };
+
+            point[] ps2 = new point[]
+            {
+                new point(point.type_point.st, 10, 10),
+                new point(point.type_point.spec, 20, 20),
+                new point(point.type_point.spec, 30, 30),
+                new point(point.type_point.fin, 40, 40)
+            };
+
+            curve[] cs = new curve[2];
+            cs[0] = new curve(curve.type_line.line);
+            cs[0].points = ps1;
+            cs[1] = new curve(curve.type_line.bezier);
+            cs[1].points = ps2;
+
+            poligon[] poligs = new poligon[1];
+            poligs[0] = new poligon();
+            poligs[0].curves = cs;
+
+
+            gmesh gm = new gmesh();
+            gm.poligons = poligs;
+
+            
+
+
+
+            SoapFormatter xml_out = new SoapFormatter();
+            f = File.Create("xml_out.xml");
+            xml.Serialize(f, gm);
+            f.Close();
+        }
+    }
+}
---
+++ Project/Parser/Parser/Parser/gmesh.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Parser_050416
+{
+    [Serializable]
+    public class gmesh
+    {
+        [XmlArrayItem]
+        public poligon[] poligons;
+
+        public gmesh() { }
+    }
+
+    [Serializable]
+    public class poligon
+    {
+        [XmlArrayItem]
+        public curve[] curves;
+
+        public poligon() { }
+    }
+
+    [Serializable]
+    public class curve
+    {
+        public enum type_line { line, bezier };
+        [XmlAttribute]
+        public type_line type;
+        [XmlArrayItem]
+        public point[] points;
+
+
+        public curve() { }
+        public curve(type_line type) 
+        {
+            this.type = type;
+        }
+    }
+
+    [Serializable]
+    public class point
+    {
+        [XmlAttribute]
+        public int x;
+        [XmlAttribute]
+        public int y;
+        public enum type_point { st, spec, fin };
+        [XmlAttribute]
+        public type_point type;
+
+        public point() { }
+        public point(type_point type, int x, int y)
+        {
+            this.type = type;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
