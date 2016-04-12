54f63d4db46be9837c824ccfe398d084935e82d1
alexklimov <klimwork93@mail.ru>
2016-04-12 23:28:52 +0300

    создал класс QuadGen.cs - генератор четырехугольника

---
+++ Project/GMESH_10/Solvers/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Geometry;
+
+namespace Solvers
+{
+    public interface IMeshGen
+    {
+        Point[,] Generate(List<ICurve> curves);
+    }
+}
