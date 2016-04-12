4c489e7d528797fe777784e6f888ca548f818824
AlexFedotov <alesha.school127@bk.ru>
2016-04-12 18:50:43 +0400

    Added class Morph

---
+++ Project/GMESH_10/Geometry/Morph.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geometry
+{
+    public class Morph : ICurve
+    {
+        private ICurve a;
+        private ICurve b;
+        private double alpha;
+
+        public Morph(ICurve a, ICurve b, double alpha)
+        {
+            this.a = a;
+            this.b = b;
+            this.alpha = alpha;
+        }
+
+        public void getPoint(double t, out double x, out double y)
+        {
+            double ax, ay, bx, by;
+
+            a.getPoint(t, out ax, out ay);
+            b.getPoint(t, out bx, out by);
+
+            x = this.alpha * ax + (1 - this.alpha) * bx;
+            y = this.alpha * ay + (1 - this.alpha) * by;
+        }
+    }
+}
