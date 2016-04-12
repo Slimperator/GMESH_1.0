5e5050941551129b6b50474b68d1ffc3d414ae43
AlexFedotov <alesha.school127@bk.ru>
2016-04-12 18:51:47 +0400

    Added class Relocate

---
+++ Project/GMESH_10/Geometry/Relocate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geometry
+{
+    public class Relocate : ICurve
+    {
+        private Point newA, newB;
+        private ICurve curve;
+
+        public Relocate(ICurve curve, Point newA, Point newB)
+        {
+            this.newA = newA;
+            this.newB = newB;
+            this.curve = curve;
+        }
+
+        public void getPoint(double t, out double x, out double y)
+        {
+            double ax, ay, bx, by;
+            this.curve.getPoint(0, out ax, out ay);
+            this.curve.getPoint(1, out bx, out by);
+            this.curve.getPoint(t, out x, out y);
+            x += (1 - t) * (this.newA.X - ax) + t * (this.newB.X - bx);
+            y += (1 - t) * (this.newA.Y - ay) + t * (this.newB.Y - by);
+        }
+    }
+}
