bcaa7a91c640e3f71eeb54bc82d17d128c48a7b8
AlexFedotov <alesha.school127@bk.ru>
2016-04-17 09:49:32 +0400

    Added class RegMesh2D

---
+++ Project/GMESH_10/Geometry/RegMesh2D.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geometry
+{
+    public class RegMesh2D
+    {
+        private Point[,] mesh;
+        private int nX;
+        private int nY;
+
+        /// <summary>
+        /// Обращение к точке из сетки по индексу
+        /// </summary>
+        /// <param name="i">Номер строки</param>
+        /// <param name="j">Номер столбца</param>
+        /// <returns>Объект "точка", лежащий в данной индексированной ячейке</returns>
+        public Point this[int i, int j]
+        {
+            get
+            {
+                return new Point(0, 0);
+            }
+            set { }
+        }
+    }
+}
