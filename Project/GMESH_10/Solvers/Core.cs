80dd2c83b5c9746f40207eec5c15482e7152a1a3
AlexFedotov <alesha.school127@bk.ru>
2016-04-17 09:51:56 +0400

    Added interfaces IGrade, IContourDecompositor.

--- Project/GMESH_10/Solvers/Core.cs
+++ Project/GMESH_10/Solvers/Core.cs
@@ -6,8 +6,30 @@ using Geometry;
 
 namespace Solvers
 {
+    /// <summary>
+    /// Интерфейс, описывающий внешние контракты генераторов сеток
+    /// </summary>
     public interface IMeshGen
     {
-        Point[,] Generate(List<ICurve> curves);
+        Point[,] Generate(Contour contour);
+    }
+    /// <summary>
+    /// Интерфейс, описывающий внешние контракты декомпозиторов сеток
+    /// </summary>
+    public interface IContourDecompositor
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="contour"></param>
+        /// <returns></returns>
+        IContour[] decomposed(IContour contour);
+    }
+    /// <summary>
+    /// Интерфейс, описывающий внешние контракты для оценки качества сетки
+    /// </summary>
+    public interface IGrade
+    {
+
     }
 }
