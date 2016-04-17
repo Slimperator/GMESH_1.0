e53b830acf774c794875c353f4e3ae8e8cbc8e6c
AlexFedotov <alesha.school127@bk.ru>
2016-04-17 10:14:28 +0400

    Rename QuadGen.cs to QuadSimpleMeshGen.cs

---
+++ Project/GMESH_10/Solvers/QuadSimpleMeshGen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Geometry;
+
+namespace Solvers
+{
+    public class QuadSimpleMeshGen : IMeshGen
+    {
+        private int nX;
+        private int nY;
+        private double hX;
+        private double hY;
+
+        public QuadSimpleMeshGen (int nX,int nY)
+        {
+            
+        }
+
+        public Point[,] Generate(Contour contour)
+        {
+            // Раньше мы принимали массив кривых "curves"
+            // Теперь он инкапсюлирован в контуре. 
+            // Надо переделать согласно такой конструкции не меняя сам алгоритм
+            return null;
+        }
+    }
+}
