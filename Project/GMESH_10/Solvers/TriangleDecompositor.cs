e44f608bde5dfd65eefa75901906a66677ddbf98
AlexFedotov <alesha.school127@bk.ru>
2016-04-17 10:15:51 +0400

    Added TriangleDecompositor.cs

---
+++ Project/GMESH_10/Solvers/TriangleDecompositor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Geometry;
+
+namespace Solvers
+{
+    public class TriangleDecompositor : IContourDecompositor
+    {
+        public IContour[] decomposed(IContour contour)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
