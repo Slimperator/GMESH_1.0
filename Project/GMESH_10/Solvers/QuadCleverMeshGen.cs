9fd14b5324465d50f377c351f6f27dd068aa4765
AlexFedotov <alesha.school127@bk.ru>
2016-04-17 10:07:47 +0400

    Added QuadCleverMeshGen.cs

---
+++ Project/GMESH_10/Solvers/QuadCleverMeshGen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Geometry;
+
+namespace Solvers
+{
+    /// <summary>
+    /// "Умный" генератор сетки для четырехугольников. В алгоритмах генерации использует "Tools".
+    /// </summary>
+    public class QuadCleverMeshGen : IMeshGen
+    {
+        public Point[,] Generate(Contour contour)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
