a271b4bdb4259160b56b668589b3f102ed45d8fe
AlexFedotov <alesha.school127@bk.ru>
2016-04-17 10:05:36 +0400

    Added Tools.cs

---
+++ Project/GMESH_10/Solvers/Tools.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Geometry;
+
+namespace Solvers
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public class Tools
+    {
+        public static double length(ICurve curve)
+        {
+            return 0;
+        }
+        public static double getParam(ICurve curve, double length)
+        {
+            return 0;
+        }
+    }
+}
