04c3716039ed8bd1445afaba00a658cfae7be72f
AlexFedotov <alesha.school127@bk.ru>
2016-04-17 09:46:27 +0400

    Added interface IContour. Added class Contour

---
+++ Project/GMESH_10/Geometry/Contour.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geometry
+{
+    public class Contour : IContour
+    {
+        private List<ICurve> curves;
+
+        public Contour(List<ICurve> curves)
+        {
+            this.curves = curves;
+        }
+
+        public int Size
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public ICurve this[int i]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                // Пока не нужно
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
--- Project/GMESH_10/Geometry/Core.cs
+++ Project/GMESH_10/Geometry/Core.cs
@@ -6,21 +6,35 @@ using System.Threading.Tasks;
 
 namespace Geometry
 {
+    /// <summary>
+    /// Интерфейс определяет внешние контракты объекта "Точка"
+    /// </summary>
     public interface IPoint
     {
         double X { get; set; }
         double Y { get; set; }
     }
-
+    /// <summary>
+    /// Интерфейс определяет внешние контракты объекта "Отрезок"
+    /// </summary>
     public interface ISegment : ICurve
     {
         IPoint St { get; set; }
         IPoint Fin { get; set; }
     }
-
-
+    /// <summary>
+    /// Интерфейс определяет внешние контракты объекта "Кривая"
+    /// </summary>
     public interface ICurve
     {
         void getPoint(double t, out double x, out double y);
+    }
+    /// <summary>
+    /// Интерфейс определяет внешние контракты объекта "Контур"
+    /// </summary>
+    public interface IContour
+    {
+        int Size { get; }
+        ICurve this[int i] { get; set; }
     }
 }
