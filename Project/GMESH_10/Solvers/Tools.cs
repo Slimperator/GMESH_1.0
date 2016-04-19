38b4ecff8763c8d9d4b2394d8e324226ac921bf3
AlexFedotov <alesha.school127@bk.ru>
2016-04-19 14:17:39 +0400

    Added methods in Tools

--- Project/GMESH_10/Solvers/Tools.cs
+++ Project/GMESH_10/Solvers/Tools.cs
@@ -9,15 +9,26 @@ namespace Solvers
     /// <summary>
     ///
     /// </summary>
-    public class Tools
+    public static class Tools
     {
+        private static double h = 0.001;
+
         public static double length(ICurve curve)
         {
-            return 0;
+            double x1, y1, x2, y2;
+            double result = 0.0;
+            for (double t = 0; t < 1; t += h)
+            {
+                curve.getPoint(t, out x1, out y1);
+                curve.getPoint(t + h, out x2, out y2);
+                ISegment temp = new Segment(new Point(x1, y1), new Point(x2, y2));
+                result += temp.Length;
+            }
+            return result;
         }
         public static double getParam(ICurve curve, double length)
         {
-            return 0;
+            return length/Tools.length(curve);
         }
     }
 }
