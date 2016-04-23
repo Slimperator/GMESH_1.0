00b0d0518221bed3759b18d5d4628942b02fd498
Daria <gorschkova15daria@yandex.ru>
2016-04-23 17:14:50 +0300

    изменила класс Bezier.cs

--- Project/GMESH_10/Geometry/Bezier.cs
+++ Project/GMESH_10/Geometry/Bezier.cs
@@ -9,17 +9,17 @@ namespace Geometry
 {
     public class Bezier : ICurve
     {
-        private IPoint P0;
-        private IPoint P1;
-        private IPoint P2;
-        private IPoint P3;
+        public IPoint P0 { get; private set; }
+        public IPoint P1 { get; private set; }
+        public IPoint P2 { get; private set; }
+        public IPoint P3 { get; private set; }
 
         public Bezier(IPoint P0, IPoint P1, IPoint P2, IPoint P3)
         {
-            this.P0 = P0;
-            this.P1 = P1;
-            this.P2 = P2;
-            this.P3 = P3;
+            this.P0 = new Point(P0.X, P0.Y);
+            this.P1 = new Point(P1.X, P1.Y);
+            this.P2 = new Point(P2.X, P2.Y);
+            this.P3 = new Point(P3.X, P3.Y);
         }
 
         public void getPoint(double t, out double x, out double y)
@@ -28,10 +28,11 @@ namespace Geometry
             x = this.P0.X * (1 - t) * (1 - t) * (1 - t) + 3 * t * P1.X * (1 - t) * (1 - t) + 3 * t * t * P2.X * (1 - t) + t * t * t * P3.X;
             y = this.P0.Y * (1 - t) * (1 - t) * (1 - t) + 3 * t * P1.Y * (1 - t) * (1 - t) + 3 * t * t * P2.Y * (1 - t) + t * t * t * P3.Y;
         }
-
-        public void accept(IVisitor visitor)
+        public void aboutCurve(out string type, out List<IPoint> Points, out List<string> Special)
         {
-            visitor.visitBezier(this);
+            type = "bezier";
+            Points = new List<IPoint>() { P0, P1, P2, P3 };
+            Special = new List<string>();
         }
     }
 }
