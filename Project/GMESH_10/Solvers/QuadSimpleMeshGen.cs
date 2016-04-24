c483aade6a7eb024f6e301a480fc31589251f413
AlexFedotov <alesha.school127@bk.ru>
2016-04-24 21:51:44 +0400

    Fix bag in QuadSimpleMeshGen

--- Project/GMESH_10/Solvers/QuadSimpleMeshGen.cs
+++ Project/GMESH_10/Solvers/QuadSimpleMeshGen.cs
@@ -13,42 +13,41 @@ namespace Solvers
         private double hX;
         private double hY;
 
+        /// <summary>
+        /// Конструктор "простого" генератора сетки
+        /// </summary>
+        /// <param name="nX">Количество разбиений по горизонатали(относительно первой кривой)</param>
+        /// <param name="nY">Количество разбиений по вертикали(относительно второй кривой)</param>
         public QuadSimpleMeshGen(int nX, int nY)
         {
-            this.nX = nX + 2;
-            this.nY = nY + 2;
-            this.hX = 1.0 / (double)(nX + 1);
-            this.hY = 1.0 / (double)(nY + 1);
+            this.nX = nX + 1;
+            this.nY = nY + 1;
+            this.hX = 1.0 / (double)(nX);
+            this.hY = 1.0 / (double)(nY);
         }
 
         public RegMesh2D Generate(Contour contour)
         {
-
-            if (contour.Size != 4)
-            {
-                return null;
-            }
-
             IPoint[,] points = new Point[this.nY, this.nX];
             ICurve a1 = contour[0];
             ICurve a2 = contour[1];
             ICurve a3 = contour[2];
             ICurve a4 = contour[3];
 
-            for (int i = 0; i <= this.nY; i++)
+            for (int i = 0; i < this.nY; i++)
             {
                 double xA, yA, xB, yB;
-                a4.getPoint(i * hY, out xA, out yA);
+                a4.getPoint(1 - i * hY, out xA, out yA);
                 a2.getPoint(i * hY, out xB, out yB);
                 ICurve curve = new Relocate(new Morph(a1, a3, i * hY), new Point(xA, yA), new Point(xB, yB));
-                for (int j = 0; j <= this.nX; j++)
+                for (int j = 0; j < this.nX; j++)
                 {
                     double x, y;
                     curve.getPoint(j * hX, out x, out y);
                     points[i, j] = new Point(x, y);
                 }
             }
-            return new RegMesh2D(points, nY, nX);
+            return new RegMesh2D(points, nX, nY);
         }
     }
 }
