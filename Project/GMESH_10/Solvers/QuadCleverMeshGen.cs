ac9c8e816389a8f637d379ab9ddb72746629399f
Slimperator <slimperator@gmail.com>
2016-04-25 02:35:17 +0300

    fix QuadCleverMeshGen. Now worked

--- Project/GMESH_10/Solvers/QuadCleverMeshGen.cs
+++ Project/GMESH_10/Solvers/QuadCleverMeshGen.cs
@@ -13,15 +13,16 @@ namespace Solvers
     {
         private Contour contour;
         private RegMesh2D mesh;
-        private double lenghtOfPart, alpha;
-        private int numOfCurvs, step;
+        private double lenghtOfPart, hX, hY;
+        private int numOfCurvs, nX, nY;
         private double[] vectorOfParam;
-        private Point[,] Points;
-        public QuadCleverMeshGen(int step, double alpha)
+        public QuadCleverMeshGen(int nX, int nY)
         {
-            this.step = step;
-            this.alpha = alpha;
-            vectorOfParam = new double[step + 1];
+            this.nX = nX + 1;
+            this.nY = nY + 1;
+            this.hX = 1.0 / (double)(nX);
+            this.hY = 1.0 / (double)(nY);
+            vectorOfParam = new double[this.nX];
         }
         public RegMesh2D Generate(Contour contour)
         {
@@ -32,7 +33,7 @@ namespace Solvers
                 case 4:
                     lenghtOfPart = getSizeOfPart(Tools.length(contour[0]));
                     createVectorOfParam();
-                    mesh = new RegMesh2D(Convert.ToInt32(Math.Floor(1 / alpha)), step + 1);
+                    mesh = new RegMesh2D(nY, nX);
                     fillMesh();
                     return mesh;
                 default:
@@ -41,12 +42,12 @@ namespace Solvers
         }
         private double getSizeOfPart(double lenghtOfCurve)
         {
-            return lenghtOfCurve / step;
+            return lenghtOfCurve * hX;
         }
         private void createVectorOfParam()
         {
             double intermediate_lenght = 0;
-            for (int i = 0; i < step + 1; i++)
+            for (int i = 0; i < nX; i++)
             {
                 vectorOfParam[i] = Tools.getParam(contour[0], intermediate_lenght);
                 intermediate_lenght += lenghtOfPart;
@@ -54,17 +55,17 @@ namespace Solvers
         }
         private void fillMesh()
         {
-            for (int i = 0; i < Math.Floor(1 / alpha); i++)
+            for (int i = 0; i < nY; i++)
             {
                 Point a, b;
                 double x, y;
-                contour[1].getPoint(1 - alpha * i, out x, out y);
+                contour[1].getPoint(1 - hY * i, out x, out y);
                 a = new Geometry.Point(x, y);
-                contour[3].getPoint(alpha * i, out x, out y);
+                contour[3].getPoint(hY * i, out x, out y);
                 b = new Geometry.Point(x, y);
-                ICurve curve = new Relocate(new Morph(contour[0], contour[2], alpha * i), a, b);
+                ICurve curve = new Relocate(new Morph(contour[0], contour[2], hY * i), a, b);
 
-                for (int j = 0; j < step + 1; j++)
+                for (int j = 0; j < nX; j++)
                 {
                     curve.getPoint(vectorOfParam[j], out x, out y);
                     mesh[i, j].X = x;
