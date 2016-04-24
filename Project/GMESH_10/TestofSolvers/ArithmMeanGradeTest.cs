ed14b4cc5ddd9216624446e9f9b15a2cb2be94b5
Ani <kirakosyan.ani@mail.ru>
2016-04-24 14:18:46 +0300

    correct mistakes

---
+++ Project/GMESH_10/TestofSolvers/ArithmMeanGradeTest.cs
@@ -0,0 +1,112 @@
+﻿using Solvers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Geometry;
+
+namespace TestOfSolvers
+{
+    
+    
+    /// <summary>
+    ///Это класс теста для ArithmMeanGradeTest, в котором должны
+    ///находиться все модульные тесты ArithmMeanGradeTest
+    ///</summary>
+    [TestClass()]
+    public class ArithmMeanGradeTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Получает или устанавливает контекст теста, в котором предоставляются
+        ///сведения о текущем тестовом запуске и обеспечивается его функциональность.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Дополнительные атрибуты теста
+        // 
+        //При написании тестов можно использовать следующие дополнительные атрибуты:
+        //
+        //ClassInitialize используется для выполнения кода до запуска первого теста в классе
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //ClassCleanup используется для выполнения кода после завершения работы всех тестов в классе
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //TestInitialize используется для выполнения кода перед запуском каждого теста
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //TestCleanup используется для выполнения кода после завершения каждого теста
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///Тест для Конструктор ArithmMeanGrade
+        ///</summary>
+        [TestMethod()]
+        public void ArithmMeanGradeConstructorTest()
+        {
+            ArithmMeanGrade target = new ArithmMeanGrade();
+            Assert.Inconclusive("TODO: реализуйте код для проверки целевого объекта");
+        }
+
+        /// <summary>
+        ///Тест для Calculate
+        ///</summary>
+        [TestMethod()]
+        public void CalculateTest()
+        {
+            ArithmMeanGrade target = new ArithmMeanGrade(); // TODO: инициализация подходящего значения
+            RegMesh2D mesh = null; // TODO: инициализация подходящего значения
+            double expected = 0F; // TODO: инициализация подходящего значения
+            double actual;
+            actual = target.Calculate(mesh);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Проверьте правильность этого метода теста.");
+        }
+
+        /// <summary>
+        ///Тест для CalculateSquare
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("Solvers.exe")]
+        public void CalculateSquareTest()
+        {
+            ArithmMeanGrade_Accessor target = new ArithmMeanGrade_Accessor(); // TODO: инициализация подходящего значения
+            IPoint p1 = null; // TODO: инициализация подходящего значения
+            IPoint p2 = null; // TODO: инициализация подходящего значения
+            IPoint p3 = null; // TODO: инициализация подходящего значения
+            IPoint p4 = null; // TODO: инициализация подходящего значения
+            double expected = 0F; // TODO: инициализация подходящего значения
+            double actual;
+            actual = target.CalculateSquare(p1, p2, p3, p4);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Проверьте правильность этого метода теста.");
+        }
+    }
+}
---
+++ Project/GMESH_10/TestofSolvers/ToolsTest.cs
@@ -0,0 +1,108 @@
+﻿using Solvers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Geometry;
+
+namespace TestOfSolvers
+{
+    
+    
+    /// <summary>
+    ///Это класс теста для ToolsTest, в котором должны
+    ///находиться все модульные тесты ToolsTest
+    ///</summary>
+    [TestClass()]
+    public class ToolsTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Получает или устанавливает контекст теста, в котором предоставляются
+        ///сведения о текущем тестовом запуске и обеспечивается его функциональность.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Дополнительные атрибуты теста
+        // 
+        //При написании тестов можно использовать следующие дополнительные атрибуты:
+        //
+        //ClassInitialize используется для выполнения кода до запуска первого теста в классе
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //ClassCleanup используется для выполнения кода после завершения работы всех тестов в классе
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //TestInitialize используется для выполнения кода перед запуском каждого теста
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //TestCleanup используется для выполнения кода после завершения каждого теста
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///Тест для getParam
+        ///</summary>
+        [TestMethod()]
+        public void getParamTest()
+        {
+            ICurve curve = new Line(new Point(0, 0), new Point(0, 0)); // TODO: инициализация подходящего значения
+            double length = 0F; // TODO: инициализация подходящего значения
+            double expected = 0F; // TODO: инициализация подходящего значения
+            double actual;
+            actual = Tools.getParam(curve, length);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Проверьте правильность этого метода теста.");
+        }
+
+        /// <summary>
+        ///Тест для length
+        ///</summary>
+        [TestMethod()]
+        public void lengthTest()
+        {
+            ICurve curve = new Line(new Point(0, 0), new Point(1, 0)); // TODO: инициализация подходящего значения
+            double expected = 1F; // TODO: инициализация подходящего значения
+            double actual;
+            actual = Tools.length(curve);
+            Assert.AreEqual(expected, actual);
+            //  Assert.Inconclusive("Проверьте правильность этого метода теста.");
+
+        }
+             [TestMethod()]
+        public void lengthTest1()
+        {
+            ICurve curve = new Line(new Point(0, 0), new Point(0, 8)); // TODO: инициализация подходящего значения
+            double expected = 8F; // TODO: инициализация подходящего значения
+            double actual;
+            actual = Tools.length(curve);
+            Assert.AreEqual(expected, actual, 0.001);
+            //  Assert.Inconclusive("Проверьте правильность этого метода теста.");
+        }
+    }
+}
