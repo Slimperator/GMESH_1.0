407856aaadeffa0cc0494562ff7d09b50379e92b
Slimperator <slimperator@gmail.com>
2016-04-27 22:38:01 +0300

    add new realization of TriangleDecompositor create by Lena

--- Project/GMESH_10/Solvers/TriangleDecompositor.cs
+++ Project/GMESH_10/Solvers/TriangleDecompositor.cs
@@ -9,51 +9,41 @@ namespace Solvers
     public class TriangleDecompositor : IContourDecompositor, IVisitor
     {
         private Geometry.IPoint[] pointsOfcurve;
+        private const double PART = 0.5;
 
         public IContour[] decomposed(IContour contour)
         {
-            Geometry.IPoint[] newPoints = new Geometry.Point[contour.Size]; //тут чисто середины линий
-            List<IContour> decFigures = new List<IContour>();
+            
+            Geometry.IPoint[] newPoints = new Geometry.Point[contour.Size]; //пустой массив для середин линий
+            List<IContour> decFigures = new List<IContour>(); //пустая коллекция для фигур
             
 
             for (int i = 0; i < contour.Size; ++i) //находим середины линий
             {
-                double x = 0;
-                double y = 0;
+                double x, y;
 
-                contour[i].getPoint(0.5, out x, out y);
+                contour[i].getPoint(PART, out x, out y);
                 newPoints[i] = new Geometry.Point(x, y);
             }
 
+            //поиск точки центра масс
             Geometry.IPoint L = new Geometry.Point((newPoints[0].X + newPoints[1].X + newPoints[2].X) / 3, (newPoints[0].Y + newPoints[1].Y + newPoints[2].Y) / 3);
-            //выше поиск точки центра масс
+            
             for (int i = 0; i < contour.Size; ++i) //что это должно делать
             {
-                contour[i].accept(this); //получаем точки
-                pointsOfcurve[1] = newPoints[i];// меняем точку конца кривой на точку середины кривой
-                pointsOfcurve[2] = L;// добавляем центр масс для всего треугольника
-                if (i == 0)
-                {
-                    pointsOfcurve[3] = newPoints[contour.Size - 1];
-                }
-                else
-                {
-                    pointsOfcurve[3] = newPoints[i - 1];
-                }
-
                 List<ICurve> lines = new List<ICurve>();
+                int index = i == 0 ? newPoints.Length - 1 : i - 1;
 
-                for (int j = 0; j < 4; ++j)
-                {
-                    if (j == 3) lines.Add(new Line(pointsOfcurve[j], pointsOfcurve[0]));
-                    else
-                    lines.Add(new Line(pointsOfcurve[j], pointsOfcurve[j + 1]));
-                }
+                lines.Add(new SubCurve(contour[i], 0, PART));
+                lines.Add(new Line(newPoints[i], L));
+                lines.Add(new Line(L, newPoints[index]));
+                lines.Add(new SubCurve(contour[index], PART, 1));
+
                 decFigures.Add(new Contour(lines)); //создаем новый контур на основе полученных линий
             }
 
             return decFigures.ToArray();
-        }
+}
 
         public void visitLine(Line curve)
         {
