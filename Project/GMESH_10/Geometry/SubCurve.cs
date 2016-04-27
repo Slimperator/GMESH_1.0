ae41c998af1749dee1e140445b8de835b03f8975
Slimperator <slimperator@gmail.com>
2016-04-27 22:36:54 +0300

    add SubCurve Decorator

---
+++ Project/GMESH_10/Geometry/SubCurve.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geometry
+{
+    public class SubCurve: ICurve
+    {
+        private ICurve mainCurve;
+        public Point begin {get; private set; }
+        public Point end { get; private set; }
+        private double paramBegin, paramEnd, max, min;
+
+        public SubCurve(ICurve mainCurve, double paramTBegin, double paramTEnd)
+        {
+            double x, y;
+            this.mainCurve = mainCurve;
+            this.paramBegin = paramTBegin;
+            this.paramEnd = paramTEnd;
+            this.mainCurve.getPoint(paramBegin,out x,out y);
+            begin = new Point(x,y);
+            this.mainCurve.getPoint(paramEnd, out x, out y);
+            end = new Point(x, y);
+            if (paramBegin > paramEnd) { max = paramBegin; min = paramEnd; }
+            if (paramBegin < paramEnd) { max = paramEnd; min = paramBegin; }
+        }
+        public void getPoint(double t, out double x, out double y)
+        {
+            this.mainCurve.getPoint(t * (max - min) + min, out x, out y);
+        }
+
+        public void accept(IVisitor visitor)
+        {
+            visitor.visit(this);
+        }
+    }
+}
