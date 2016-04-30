fb51c87842b30f941ff699872fe93b0711e2b434
alexklimov <klimwork93@mail.ru>
2016-04-30 15:37:36 +0300

    add new class Gradient

---
+++ Project/GMESH_10/Solvers/Gradient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing; // директива для Color
+
+namespace Solvers
+{
+   public class Gradient
+    {
+       double status; // оценка квадратика
+
+       Color GetCellColor(double status)
+       {
+           if (status > 1) status = 1;
+           if (status < 0) status = 0;
+
+           int G = (int)(255.0 * status);
+           int R = 255 - G;
+           int B = 0;
+           int A = 255;
+
+           return Color.FromArgb(A, R, G, B);
+       }
+       
+    }
+}
