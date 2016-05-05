86a8b9cf279ad70cd365d7ed6e52ff7425c0c7e6
Slimperator <slimperator@gmail.com>
2016-05-05 15:41:32 +0300

    fix bug in PentagonDecSquare With Ani

--- Project/GMESH_10/Solvers/PentagonDecSquare.cs
+++ Project/GMESH_10/Solvers/PentagonDecSquare.cs
@@ -34,17 +34,19 @@ namespace Solvers
             preDecompose.Add((new Line(pointsOfCurvs[1], pointsOfCurvs[3])));//2
             preDecompose.Add((new Line(pointsOfCurvs[1], pointsOfCurvs[4])));//3
             preDecompose.Add((new Line(pointsOfCurvs[2], pointsOfCurvs[4])));//4
+
             generalPoints.Add(findGeneralPoints(preDecompose[0], preDecompose[3]));//1
             generalPoints.Add(findGeneralPoints(preDecompose[0], preDecompose[2]));//2
             generalPoints.Add(findGeneralPoints(preDecompose[4], preDecompose[2]));//3
             generalPoints.Add(findGeneralPoints(preDecompose[1], preDecompose[4]));//4
-            generalPoints.Add(findGeneralPoints(preDecompose[2], preDecompose[1]));//5
-            finalDecompose.Add(new Contour(new List<ICurve> { contour[0], new Line(pointsOfCurvs[0], generalPoints[4]), new Line(pointsOfCurvs[4], pointsOfCurvs[0]) }));
-            finalDecompose.Add(new Contour(new List<ICurve> { contour[1], new Line(pointsOfCurvs[2], generalPoints[0]), new Line(generalPoints[0], pointsOfCurvs[1]) }));
+            generalPoints.Add(findGeneralPoints(preDecompose[3], preDecompose[1]));//5
+
+            //finalDecompose.Add(new Contour(new List<ICurve> { contour[0], new Line(pointsOfCurvs[1], generalPoints[4]), new Line(generalPoints[4], pointsOfCurvs[0]) }));
+            //finalDecompose.Add(new Contour(new List<ICurve> { contour[1], new Line(pointsOfCurvs[2], generalPoints[0]), new Line(generalPoints[0], pointsOfCurvs[1]) }));
             finalDecompose.Add(new Contour(new List<ICurve> { contour[2], new Line(pointsOfCurvs[3], generalPoints[3]), new Line(generalPoints[3], generalPoints[1]), new Line(generalPoints[1], pointsOfCurvs[2]) }));
-            finalDecompose.Add(new Contour(new List<ICurve> { contour[3], new Line(pointsOfCurvs[4], generalPoints[3]), new Line(generalPoints[3], pointsOfCurvs[3]) }));
-            finalDecompose.Add(new Contour(new List<ICurve> { contour[4], new Line(pointsOfCurvs[2], generalPoints[0]), new Line(generalPoints[0], pointsOfCurvs[1]) }));
-            finalDecompose.Add(new Contour(new List<ICurve> { new Line(generalPoints[0], generalPoints[1]), new Line(generalPoints[1], generalPoints[2]), new Line(generalPoints[2], generalPoints[4]), new Line(generalPoints[4], generalPoints[0]) }));
+            //finalDecompose.Add(new Contour(new List<ICurve> { contour[3], contour[4], new Line(pointsOfCurvs[0], pointsOfCurvs[3]) }));
+            //finalDecompose.Add(new Contour(new List<ICurve> { new Line(generalPoints[0], generalPoints[1]), new Line(generalPoints[1], generalPoints[3]), new Line(generalPoints[3], generalPoints[4]), new Line(generalPoints[4], generalPoints[0]) }));
+            
             return finalDecompose.ToArray();
         }
 
@@ -55,8 +57,9 @@ namespace Solvers
             l1.getPoint(1, out x2, out y2);
             l2.getPoint(0, out x3, out y3);
             l2.getPoint(1, out x4, out y4);
-            double x = ((x1 * y2 - x2 * y1) * (x4 - x3) - (x3 * y4 - x4 * y3) * (x2 - x1)) / ((y1 - y2) * (x4 - x3) - (y3 - y4) * (x2 - x1));
-            double y = ((y3 - y4) * x - (x3 * y4 - x4 * y3)) / (x4 - x3);
+            double ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+            double x = x1 + ua * (x2 - x1);
+            double y = y1 + ua * (y2 - y1);
 
             return new Point(x, y);
         }
