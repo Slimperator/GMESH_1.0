112ad930e7016c17f2d60c1bad8202f9493a1eaf
Elena Krotova <ekrotovva@gmail.com>
2016-05-06 03:13:33 +0300

    Оптимизация треугольника

--- Project/GMESH_10/Solvers/TriangleDecompositor.cs
+++ Project/GMESH_10/Solvers/TriangleDecompositor.cs
@@ -10,7 +10,12 @@ namespace Solvers
     {
         private Geometry.IPoint[] pointsOfcurve;
         private const double PART = 0.5;
-
+        private double Max = 0;
+        private double alpha = 0.1;
+        private IPoint MaxPoint;
+        private bool flag = false;
+        private IMeshGen gen = new QuadCleverMeshGen(10);
+        private IGrade grade = new ArithmMeanGrade();
         public IContour[] decomposed(IContour contour)
         {
             
@@ -28,7 +33,7 @@ namespace Solvers
 
             //поиск точки центра масс
             Geometry.IPoint L = new Geometry.Point((newPoints[0].X + newPoints[1].X + newPoints[2].X) / 3, (newPoints[0].Y + newPoints[1].Y + newPoints[2].Y) / 3);
-            
+            MaxPoint = L;
             for (int i = 0; i < contour.Size; ++i) //что это должно делать
             {
                 List<ICurve> lines = new List<ICurve>();
@@ -38,8 +43,77 @@ namespace Solvers
                 lines.Add(new Line(newPoints[i], L));
                 lines.Add(new Line(L, newPoints[index]));
                 lines.Add(new SubCurve(contour[index], PART, 1));
+                Max += grade.Calculate(gen.Generate(new Contour(lines))[0]);
+            }
+            Max = Max / contour.Size;
 
-                decFigures.Add(new Contour(lines)); //создаем новый контур на основе полученных линий
+            while (flag == false)
+            {
+                double Local = 0;
+                double LocalMax = 0;
+                IPoint LocalMaxPoint = null;
+                for (int j = 0; j < 4; j++)
+                {
+                    switch (j) 
+                    {
+                        case 0:
+                            L.X = MaxPoint.X + alpha;
+                            L.Y = MaxPoint.Y;
+                            break;
+                            case 1:
+                            L.X = MaxPoint.X - alpha;
+                            L.Y = MaxPoint.Y;
+                            break;
+                            case 2:
+                            L.X = MaxPoint.X ;
+                            L.Y = MaxPoint.Y + alpha;
+                            break;
+                            case 3:
+                            L.X = MaxPoint.X ;
+                            L.Y = MaxPoint.Y - alpha;
+                            break;
+                    }
+
+                    for (int i = 0; i < contour.Size; ++i) //что это должно делать
+                    {
+                        List<ICurve> lines = new List<ICurve>();
+                        int index = i == 0 ? newPoints.Length - 1 : i - 1;
+
+                        lines.Add(new SubCurve(contour[i], 0, PART));
+                        lines.Add(new Line(newPoints[i], L));
+                        lines.Add(new Line(L, newPoints[index]));
+                        lines.Add(new SubCurve(contour[index], PART, 1));
+                        Local += grade.Calculate(gen.Generate(new Contour(lines))[0]);
+                    }
+                    Local = Local / contour.Size;
+                    if (Local > LocalMax)
+                    {
+                        LocalMax = Local;
+                        LocalMaxPoint = L;
+                        Local = 0;
+                    }
+                }
+                if (Max - LocalMax < -alpha)
+                {
+                    Max = LocalMax;
+                    MaxPoint = LocalMaxPoint;
+                }
+                else
+                {
+                    flag = true;
+                }
+            }
+
+            for (int i = 0; i < contour.Size; ++i) //что это должно делать
+            {
+                List<ICurve> lines = new List<ICurve>();
+                int index = i == 0 ? newPoints.Length - 1 : i - 1;
+
+                lines.Add(new SubCurve(contour[i], 0, PART));
+                lines.Add(new Line(newPoints[i], MaxPoint));
+                lines.Add(new Line(MaxPoint, newPoints[index]));
+                lines.Add(new SubCurve(contour[index], PART, 1));
+                decFigures.Add(new Contour(lines));
             }
 
             return decFigures.ToArray();
